ddea7514526eb22cabe8fc8c3a3db535188d3d77
GCalizaya <giordanovcc3@gmail.com>
2023-03-23 22:35:50 -0500

    ejemplos agregados

---
+++ Examples/CuentaBancaria.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{ 
+    public class CuentaBancaria
+       
+    {
+        public decimal saldo;
+        private decimal saldoDepositado;
+
+        public CuentaBancaria(decimal saldoInicial)
+        {
+            saldo = saldoInicial;
+            saldoDepositado = 0;
+        }
+
+        public void Depositar(decimal cantidad)
+        {
+            saldo += cantidad;
+            saldoDepositado += cantidad;
+        }
+        public decimal ObtenerSaldoDepositado()
+        {
+            return saldoDepositado;
+        }
+
+        public void Retirar(decimal cantidad)
+        {
+            if (cantidad <= saldo)
+            {
+                saldo -= cantidad;
+            }
+            else
+            {
+                Console.WriteLine("No tienes suficiente saldo para realizar esta operación.");
+            }
+        }
+    }
+        
+}
---
+++ Examples/Program.cs
@@ -0,0 +1,22 @@
+﻿using Examples;
+using System;
+
+namespace MiProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var cuenta = new CuentaBancaria(1400);
+            cuenta.Depositar(500);
+            cuenta.Retirar(200);
+            cuenta.Retirar(1000);
+
+            Console.WriteLine("El saldo Depositado es: " + cuenta.ObtenerSaldoDepositado());
+            Console.WriteLine("El saldo actual es: " + cuenta.saldo);
+        }
+    }
+             
+}
